7c31ab419a3809b72170381dce771d758088e58a
Alexandre Magno Monteiro <a.mmagno@yahoo.com.br>
2017-05-29 17:52:43 -0300

    testing

---
+++ server.js
@@ -0,0 +1,32 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import { graphqlExpress, graphiqlExpress } from 'graphql-server-express'
+import { makeExecutableSchema } from 'graphql-tools'
+const typeDefs = [`
+type Query {
+  hello: String
+}
+
+schema {
+  query: Query
+}`]
+
+const resolvers = {
+  Query: {
+    hello(root) {
+      return 'world'
+    }
+  }
+}
+const mySchema = makeExecutableSchema({ typeDefs, resolvers })
+const port = 4000
+
+let app = express()
+
+
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema: mySchema }))
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }))
+
+app.listen(port, () => {
+    console.log('Testing.....')
+})
